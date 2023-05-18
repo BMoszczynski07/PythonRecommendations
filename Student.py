da961eb3ead2bdd888d260792522759a38511cb1
Bartosz Moszczyński <109615293+BMoszczynski07@users.noreply.github.com>
2023-05-18 23:59:35 +0200

    working on project

--- Student.py
+++ Student.py
@@ -1,9 +1,30 @@
+from collections import Counter
+
+students_interests = []
+
+
 class Student:
-    def __init__(self, filmy, jedzenie, miejsca, muzyka):
-        self.filmy = filmy
-        self.jedzenie = jedzenie
-        self.miejsca = miejsca
-        self.muzyka = muzyka
+    def __init__(self, film, jedzenie, miejsca, muzyka, wiek):
+        self.film = film.lower()
+        self.jedzenie = jedzenie.lower()
+        self.miejsca = miejsca.lower()
+        self.muzyka = muzyka.lower()
+        self.wiek = wiek
+        self.add_student_interests()
+
+    def recommend(self):
+        # TODO: 1. Utwórz listę, która pobierze te zainteresowania studentów w students_interests, których wiek odpowiada wiekowi studenta w self.wiek
+        # TODO: 2. Usuń z listy wyniki, w których ulubiona muzyka jest taka sama jak ulubiona muzyka analizowanego studenta w self.muzyka
+        # TODO: 3. Za pomocą Countera policz w tablicy częstotliwość występowania poszczególnych gatunków muzyki w liście
+        # TODO: 4. Na końcu w funkcji zwróć tuple o wartościach (<gatunek_muzyki>, <częstotliwość_występowania>)
+        pass
+
+    def add_student_interests(self):
+        student_interests = [self.film,
+                             self.jedzenie, self.miejsca, self.muzyka]
+
+        students_interests.append(student_interests)
 
     def __str__(self):
-        print(f"Student({self.filmy}, {self.muzyka}, {self.jedzenie}, {self.miejsca})")
+        print(
+            f"Student({self.film}, {self.muzyka}, {self.jedzenie}, {self.miejsca})")
--- script.py
+++ script.py
@@ -1,2 +1,19 @@
-from collections import Counter
-from typing import List, Tuple
+from Student import Student
+
+with open("studenci.txt", "r", encoding="utf-8") as students:
+    next(students)
+
+    for student in students:
+        student_interests = student.strip().split(',')
+
+        next_student = Student(*student_interests)
+
+# Nowy student, który przyjechał do Warszawy
+# TODO: Naszym zadaniem jest polecenie naszemu studentowi nowych gatunków muzyki na podstawie pietnastu innych studentów
+nowy_student = Student("Gwiezdne Wojny", "Kurczak curry",
+                       "Złote Tarasy", "Klasyczna", 22)
+
+# TODO: tutaj pobieramy tuple z rekomendowanymi gatunkami muzyki dla nowego studenta
+recommended = nowy_student.recommend()
+
+# print(TODO: Tutaj wywołaj pętle, która przeiteruje przez otrzymaną tuple i wyświetli polecane gatunki muzyki)
